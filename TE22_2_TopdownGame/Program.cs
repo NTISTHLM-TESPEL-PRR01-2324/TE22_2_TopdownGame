49c33ad6ca2242bbe28a7c84eba0c50c6c74fd14
Mikael Bergström <mikael.bergstrom@ga.ntig.se>
2023-10-17 11:20:49 +0200

    Förflyttning av karaktären

--- TE22_2_TopdownGame/Program.cs
+++ TE22_2_TopdownGame/Program.cs
@@ -1,15 +1,24 @@
 ﻿using Raylib_cs;
 using System.Numerics;
 
+// - Vektorer för förflyttning
+// - Vektorer för positioner (cirklar)
+// - Rektanglar
+// - Texturer
+// - Färger
+
+// - Input
+// - Olika scener
+// - Kollisioner
 
 Raylib.InitWindow(800, 600, "Hello");
 Raylib.SetTargetFPS(60);
 
-int x = 0;
+// int x = 0;
 int floorY = 550;
 int floorSpeedY = -1;
-Vector2 position = new Vector2(0, 0);
-Vector2 movement = new Vector2(2, 1);
+// Vector2 position = new Vector2(0, 0);
+Vector2 movement = new Vector2(0.1f, 0.1f);
 
 Color hotPink = new Color(255, 105, 180, 255);
 
@@ -19,15 +28,41 @@ Rectangle characterRect = new Rectangle(10, 10, 32, 32);
 characterRect.width = characterImage.width;
 characterRect.height = characterImage.height;
 
+float speed = 5;
+
 while (!Raylib.WindowShouldClose())
 {
-  // kod här: läsa in knapptryck, ändra på movement
+  movement = Vector2.Zero;
 
+  // kod här: läsa in knapptryck, ändra på movement
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+  {
+    movement.X = -1;
+  }
+  else if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+  {
+    movement.X = 1;
+  }
+  if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+  {
+    movement.Y = -1;
+  }
+  else if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+  {
+    movement.Y = 1;
+  }
+
+  if (movement.Length() > 0)
+  {
+    movement = Vector2.Normalize(movement);
+  }
+
+  movement *= speed;
 
   characterRect.x += movement.X;
   characterRect.y += movement.Y;
 
-  x++;
+  // x++;
   floorY += floorSpeedY;
   if (floorY < 0)
   {
@@ -38,7 +73,8 @@ while (!Raylib.WindowShouldClose())
     floorSpeedY = -1;
   }
 
-  position += movement;
+  // position += movement;
+
   Raylib.BeginDrawing();
   Raylib.ClearBackground(Color.GOLD);
 
